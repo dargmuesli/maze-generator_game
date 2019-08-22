f15718c5266d1f3936c32f5aff3ff9241d8ff37d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-08-22 16:53:56 +0200

    Add A Main Class

---
+++ src/main/java/de/jonas_thelemann/smaze/Main.java
@@ -0,0 +1,9 @@
+package de.jonas_thelemann.smaze;
+
+import greenfoot.export.GreenfootScenarioApplication;
+
+public class Main extends GreenfootScenarioApplication {
+    public static void main(String[] args) {
+        GreenfootScenarioApplication.launch(args);
+    }
+}
